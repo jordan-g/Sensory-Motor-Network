Make one demo file; Fix small bug in whisker deflection calculation

agent.py
@@ -1,14 +1,13 @@
 import numpy as np
 
 class Agent:
-    def __init__(self, n_whiskers, whisker_length, total_whiskers_angle, n_sensory_cells, sensory_cells_learning_rate):
+    def __init__(self, n_whiskers, min_whisker_length, max_whisker_length, total_whiskers_angle, n_sensory_cells, sensory_cells_learning_rate):
         self.x                    = 0
         self.n_whiskers           = n_whiskers
-        self.whisker_length       = whisker_length
         self.total_whiskers_angle = total_whiskers_angle
         self.n_sensory_cells      = n_sensory_cells
 
-        self.whiskers = Whiskers(self, self.n_whiskers, self.whisker_length, self.total_whiskers_angle)
+        self.whiskers = Whiskers(self, self.n_whiskers, min_whisker_length, max_whisker_length, self.total_whiskers_angle)
         self.sensory_cells = SensoryCells(self, self.n_sensory_cells, self.n_whiskers, sensory_cells_learning_rate)
 
     def update_whisker_deflections(self, texture):
@@ -19,24 +18,37 @@ class Agent:
         self.x += speed
         self.whiskers.origin_x = self.x
 
+    def update_whisking_angle(self, angle):
+        self.whiskers.whisking_angle = angle
+        self.whiskers.calculate_whisker_angles()
+
 class Whiskers:
-    def __init__(self, agent, n, whisker_length, total_whiskers_angle):
+    def __init__(self, agent, n, min_whisker_length, max_whisker_length, total_whiskers_angle):
         # Initialize variables
         self.agent                = agent
         self.n                    = n
         self.total_whiskers_angle = total_whiskers_angle
-        self.whisker_length       = whisker_length
+        self.whisker_lengths      = 0.2*np.exp(np.linspace(min_whisker_length, max_whisker_length, n))
         self.origin_x             = agent.x
+        self.whisking_angle       = np.pi/2
 
         self.deflections    = np.zeros(self.n)
-        self.whisker_angles = np.linspace((np.pi - total_whiskers_angle)/2, (np.pi + total_whiskers_angle)/2, self.n)
+
+        self.calculate_whisker_angles()
+
+    def calculate_whisker_angles(self):
+        self.whisker_angles = np.linspace((self.whisking_angle - self.total_whiskers_angle)/2, (self.whisking_angle + self.total_whiskers_angle)/2, self.n)
 
     def calculate_deflections(self, texture):
         y_range = np.linspace(texture.min_value(), texture.max_value(), 100)
 
         for n in range(self.n):
             whisker_angle = self.whisker_angles[n]
-            whisker_x_values = y_range/np.tan(whisker_angle)
+            tan_whisker_angle = np.tan(whisker_angle)
+            if tan_whisker_angle == 0:
+                whisker_x_values = 0*y_range
+            else:
+                whisker_x_values = y_range/tan_whisker_angle
 
             texture_y_values = texture.value(whisker_x_values + self.origin_x)
 
@@ -45,9 +57,9 @@ class Whiskers:
             if len(whisker_lengths) > 0:
                 min_whisker_length = np.amin(whisker_lengths)
             else:
-                min_whisker_length = self.whisker_length
+                min_whisker_length = self.whisker_lengths[n]
 
-            deflection = np.maximum(self.whisker_length - min_whisker_length, 0)
+            deflection = np.maximum(self.whisker_lengths[n] - min_whisker_length, 0)
             self.deflections[n] = deflection
 
 class SensoryCells:

plotting.py
@@ -7,14 +7,14 @@ import matplotlib.animation as animation
 class AnimationPlot:
     def __init__(self, x_min, y_min, x_max, y_max):
         # Create the figure
-        self.figure = plt.figure(figsize=((x_max - x_min)/(y_max - y_min), 4))
+        self.figure = plt.figure(figsize=((x_max - x_min)/(y_max - y_min), 4), facecolor='black')
 
     def create_texture(self, texture, texture_length):
         # Create texture x values
         self.texture_x = np.linspace(0, texture_length, 100*texture_length)
         
         # Plot the texture
-        self.texture_line, = self.animation_axis.plot([], [], color='#5C5C5C', lw=1)
+        self.texture_line, = self.animation_axis.plot([], [], color='white', lw=1)
 
     def create_whiskers(self, n_whiskers):
         # Initialize the list of whisker lines & colors
@@ -105,15 +105,15 @@ class AnimationPlot:
             deflection    = agent.whiskers.deflections[n]
 
             # Set starting x & y coordinates of the whisker
-            y_start = 0.2*agent.whisker_length*np.sin(whisker_angle)
-            x_start = agent.x + 0.2*agent.whisker_length*np.cos(whisker_angle)
+            y_start = 0.5*agent.whiskers.whisker_lengths[0]*np.sin(whisker_angle)
+            x_start = agent.x + 0.5*agent.whiskers.whisker_lengths[0]*np.cos(whisker_angle)
 
             # Set ending x & y coordinates of the whisker
-            x_end = agent.x + (agent.whisker_length - deflection)*np.cos(whisker_angle)
-            y_end = (agent.whisker_length - deflection)*np.sin(whisker_angle)
+            x_end = agent.x + (agent.whiskers.whisker_lengths[n] - deflection)*np.cos(whisker_angle)
+            y_end = (agent.whiskers.whisker_lengths[n] - deflection)*np.sin(whisker_angle)
 
             # Update the whisker color
-            self.whisker_colors[n] = deflection/agent.whisker_length
+            self.whisker_colors[n] = deflection/agent.whiskers.whisker_lengths[n]
 
             # Update the whisker line
             self.whisker_lines[n] = [(x_start, y_start), (x_end, y_end)]
@@ -132,7 +132,7 @@ class AnimationPlot:
             deflection = agent.whiskers.deflections[n]
 
             # Update the whisker deflection line color
-            self.whisker_deflection_colors[n] = deflection/agent.whisker_length
+            self.whisker_deflection_colors[n] = deflection/agent.whiskers.whisker_lengths[n]
 
         # Update the whisker line collection
         self.whisker_line_collection.set_array(self.whisker_colors)